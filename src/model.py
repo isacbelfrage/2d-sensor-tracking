44ffcd669979b5f897384c614e2abc2d30251cf8
isacbelfrage <isac.belfrage@hotmail.se>
2026-07-26 14:09:13 +0200

    add constant velocity object model

---
+++ src/model.py
@@ -0,0 +1,11 @@
+class MovingObject:
+    def __init__(self, x: float, y: float, vx: float, vy: float):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
+    def update(self, dt: float) -> None:
+        self.x = self.x + self.vx * dt
+        self.y = self.y + self.vy * dt
+
---
+++ tests/test_model.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.model import MovingObject
+
+class TestMovingObject(unittest.TestCase):
+    def test_update_changes_position(self):
+        # Arrange
+        obj = MovingObject(0.0, 0.0, 2.0, 1.0)
+
+        # Act
+        obj.update(0.5)
+
+        # Assert
+        self.assertAlmostEqual(obj.x, 1.0)
+        self.assertAlmostEqual(obj.y, 0.5)
+
+if __name__ == "__main__":
+    unittest.main()
