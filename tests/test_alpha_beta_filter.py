084d3d08004d26d2de7d68d279e93a47b7c163fe
isacbelfrage <isac.belfrage@hotmail.se>
2026-07-31 15:32:15 +0200

    test zero residual for alpha-beta filter

--- tests/test_alpha_beta_filter.py
+++ tests/test_alpha_beta_filter.py
@@ -61,3 +61,24 @@ class TestAlphaBetaFilter(unittest.TestCase):
         self.assertAlmostEqual(result[1], 1.1)
         self.assertAlmostEqual(ab_filter.velocity[0], 2.3)
         self.assertAlmostEqual(ab_filter.velocity[1], 0.8)
+
+    def test_update_with_no_residual(self):
+        # Arrange
+        position = (0, 0)
+        velocity = (2, 1)
+        dt = 1
+        measurement = (2, 1)
+        alpha = 0.8
+        beta = 0.2
+
+        ab_filter = AlphaBetaFilter(position, velocity, alpha, beta)
+
+        # Act
+        result = ab_filter.update(measurement, dt)
+
+        # Assert
+        self.assertEqual(result, ab_filter.position)
+        self.assertAlmostEqual(result[0], 2)
+        self.assertAlmostEqual(result[1], 1)
+        self.assertAlmostEqual(ab_filter.velocity[0], 2)
+        self.assertAlmostEqual(ab_filter.velocity[1], 1)
